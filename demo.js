31d0ff6cb6b61c77e09d05c669e994258d0e4bcd
Rufus Hamade <rufus@myfanwy.org.uk>
2022-07-25 12:12:55 +0100

    Show use of linkLanguages field

--- demo.js
+++ demo.js
@@ -6,6 +6,7 @@ import ogs from 'open-graph-scraper';
 
 const SCONE_ROOT_URL = 'https://api.scone.ai/api/v2';
 const LOCALES = ['nl-NL', 'en-US', 'de-DE', 'fr-FR', 'pt-PT']
+const LOCALE_ONLY = true
 
 const authToken = process.argv[2];
 
@@ -45,34 +46,55 @@ if (!LOCALES.includes(locale)) {
     const feedData = await feed.get({ limit: 10 });
 
     for (const feedItem of feedData.results) {
-      console.log('authorName:', feedItem.authorName)
-      console.log('authorAvatar:', feedItem.authorAvatar)
-      console.log('category:', feedItem.category)
-      console.log('contentfulId:', feedItem.contentfulId)
-      console.log('contentPublishDate:', feedItem.contentPublishDate)
-      console.log('hasPages:', feedItem.hasPages)
+      console.log()
+      console.log()
+      console.log('Getstream data:')
+      console.log(' Object ID', feedItem.object)
+      console.log(' authorName:', feedItem.authorName)
+      console.log(' authorAvatar:', feedItem.authorAvatar)
+      console.log(' category:', feedItem.category)
+      console.log(' contentfulId:', feedItem.contentfulId)
+      console.log(' contentPublishDate:', feedItem.contentPublishDate)
+      console.log(' subType', feedItem.subType)
+      console.log(' hasPages:', feedItem.hasPages)
+      console.log(' linkLanguages', feedItem.linkLanguages)
 
       if (!feedItem.contentfulId) {
-        continue;
+        console.log('Skipping rest as no contentful ID')
+        continue
+      }
+
+      if (LOCALE_ONLY && feedItem.linkLanguages && !feedItem.linkLanguages.includes(locale)) {
+        console.log('Skipping rest as language not supported')
+        continue
       }
 
       const entity = await contentfulClient.getEntry(feedItem.contentfulId, {
         include: 3,
         locale: locale
       });
-      console.log('image:', entity?.fields.image?.fields.file.url)
-      console.log('link:', entity?.fields.link)
-      console.log('linkHeadline:', entity?.fields.linkHeadline)
-      console.log('description:')
+      console.log()
+      console.log('Contentful data:')
+      console.log(' image:', entity?.fields.image?.fields.file.url)
+      console.log(' link:', entity?.fields.link)
+      console.log(' linkHeadline:', entity?.fields.linkHeadline)
+      console.log(' linkLanguages:', entity?.fields.linkLanguages)
+      console.log(' description:')
       console.log(entity?.fields.description)
       if (entity?.fields.link) {
-        const ogResult = await ogs({url: entity?.fields.link})
-        console.log('link title:', ogResult.result.ogTitle)
-        console.log('link description:', ogResult.result.ogDescription)
-        console.log('link type:', ogResult.result.ogType)
-        console.log('link image:', ogResult.result.ogImage.url)
+        console.log()
+        console.log('OpenGraph data:')
+        try {
+          const ogResult = await ogs({url: entity?.fields.link})
+          console.log(' link title:', ogResult.result.ogTitle)
+          console.log(' link description:', ogResult.result.ogDescription)
+          console.log(' link type:', ogResult.result.ogType)
+          console.log(' link image:', ogResult.result.ogImage.url)
+        } catch (e) {
+          console.log('failed to fetch')
+          continue
+        }
       }
-      console.log()
     }
   }
   catch (e) {
